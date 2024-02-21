4ffc64663323a6758e22efeddbca533a0b447542
Sabri ABDESSELEM <sabri.abdesselem@etu.iut-tlse3.fr>
2024-02-21 11:35:43 +0100

    parseXmlToArticles method adjusted

--- app/src/main/java/com/example/va_prisecommande/model/Article.kt
+++ app/src/main/java/com/example/va_prisecommande/model/Article.kt
@@ -2,12 +2,17 @@ package com.example.va_prisecommande.model
 
 data class Article(
     var ean: String,
+    var code: String,
     var nom: String,
-    var conditionnements: List<String>,
+    var conditionnements: MutableList<String> = mutableListOf(),
     var conditionnementDefaut: String? = null
-) {
+)
+
+/*
+{
     // Initialise le conditionnement par défaut lors de la création de l'objet
     init {
         conditionnementDefaut = conditionnements.firstOrNull { it.contains("default") }
     }
 }
+ */
--- app/src/main/java/com/example/va_prisecommande/singleton/DataRepository.kt
+++ app/src/main/java/com/example/va_prisecommande/singleton/DataRepository.kt
@@ -128,60 +128,47 @@ object DataRepository {
     fun parseXmlToArticles(xml: String): List<Article> {
         val cleanXml = xml.trim().removePrefix("\uFEFF")
 
-        val articles = mutableListOf<Article>()
+        val articlesMap = mutableMapOf<String, Article>()
         val xmlPullParserFactory = XmlPullParserFactory.newInstance()
         val xmlPullParser = xmlPullParserFactory.newPullParser()
         xmlPullParser.setInput(StringReader(cleanXml))
 
         var eventType = xmlPullParser.eventType
-        var currentArticle: Article? = null
-        var currentTag: String? = null
-        var currentConditionnements: MutableList<String>? = null
-        var currentConditionnementDefaut: String? = null
-        var isDefault: Boolean = false
-
         while (eventType != XmlPullParser.END_DOCUMENT) {
-            when (eventType) {
-                XmlPullParser.START_TAG -> {
-                    currentTag = xmlPullParser.name
-                    if (currentTag == "article") {
-                        currentArticle = Article("", "", mutableListOf())
-                        currentConditionnements = mutableListOf()
-                        currentConditionnementDefaut = null
-                    } else if (currentTag == "Conditionnement") {
-                        isDefault = xmlPullParser.getAttributeValue(null, "default")?.toBoolean() ?: false
+            if (eventType == XmlPullParser.START_TAG && xmlPullParser.name == "row") {
+                var ean = ""
+                var code = ""
+                var nom = ""
+                var conditionnement: String? = null
+                var principal = false
+
+                while (xmlPullParser.nextTag() == XmlPullParser.START_TAG) {
+                    val tagName = xmlPullParser.name
+                    when (tagName) {
+                        "EAN" -> ean = xmlPullParser.nextText()
+                        "Code" -> code = xmlPullParser.nextText()
+                        "Nom" -> nom = xmlPullParser.nextText()
+                        "Conditionnement" -> conditionnement = xmlPullParser.nextText()
+                        "Principal" -> principal = xmlPullParser.nextText().toInt() == 1
                     }
                 }
-                XmlPullParser.TEXT -> {
-                    val text = xmlPullParser.text.trim()
-                    if (text.isNotEmpty()) {
-                        when (currentTag) {
-                            "Ean" -> currentArticle?.ean = text
-                            "Nom" -> currentArticle?.nom = text
-                            "Conditionnement" -> {
-                                if (isDefault) {
-                                    currentConditionnementDefaut = text
-                                }
-                                currentConditionnements?.add(text)
-                            }
+
+                val article = articlesMap[ean]
+                if (article == null) {
+                    val newArticle = Article(ean, code, nom, mutableListOf(conditionnement ?: ""), conditionnement.takeIf { principal })
+                    articlesMap[ean] = newArticle
+                } else {
+                    if (conditionnement != null && !article.conditionnements.contains(conditionnement)) {
+                        article.conditionnements.add(conditionnement)
+                        if (principal) {
+                            article.conditionnementDefaut = conditionnement
                         }
                     }
-                }
-                XmlPullParser.END_TAG -> {
-                    if (xmlPullParser.name == "article" && currentArticle != null) {
-                        currentArticle.conditionnements = currentConditionnements ?: mutableListOf()
-                        currentArticle.conditionnementDefaut = currentConditionnementDefaut
-                        articles.add(currentArticle)
-                    }
-                    // Reset the default flag when the end of a Conditionnement tag is reached
-                    if (xmlPullParser.name == "Conditionnement") {
-                        isDefault = false
-                    }
-                    currentTag = null
                 }
             }
             eventType = xmlPullParser.next()
         }
-        return articles
+
+        return articlesMap.values.toList()
     }
 }
