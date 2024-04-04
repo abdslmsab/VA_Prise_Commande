d0dc4656d6a2b7a134ee859eb6a8629b079d9ff4
Sabri ABDESSELEM <sabri.abdesselem@etu.iut-tlse3.fr>
2024-04-04 17:28:09 +0200

    send the pdf to vital's mail

--- app/src/main/java/com/example/va_prisecommande/fragments/RecapitulatifFragment.kt
+++ app/src/main/java/com/example/va_prisecommande/fragments/RecapitulatifFragment.kt
@@ -1,5 +1,6 @@
 package com.example.va_prisecommande.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +15,10 @@ import com.example.va_prisecommande.utils.PathsConstants
 import com.rajat.pdfviewer.PdfViewerActivity
 import com.rajat.pdfviewer.util.saveTo
 import java.io.File
+import android.content.Intent
+import android.net.Uri
+import androidx.core.content.FileProvider
+import com.redmadrobot.inputmask.BuildConfig
 
 class RecapitulatifFragment : Fragment() {
     private var pdfName: String? = null
@@ -54,12 +59,46 @@ class RecapitulatifFragment : Fragment() {
         }
 
         binding.rightButton.setOnClickListener {
+            val file = File(PathsConstants.LOCAL_STORAGE, pdfName)
 
+            val subject = "TEST"
+            val message = "Ceci est un test"
+
+            // Envoi de l'email avec le fichier PDF en pièce jointe
+            if (file.exists()) {
+                sendEmail("commandes@vital-aine.com", subject, message, file)
+            } else {
+                Toast.makeText(requireContext(), "Fichier PDF non trouvé", Toast.LENGTH_SHORT).show()
+            }
         }
 
         view.findViewById<Button>(R.id.left_button).setOnClickListener {
             // Permet de revenir en arrière lorsque le bouton gauche est cliqué
             requireActivity().supportFragmentManager.popBackStack()
+        }
+    }
+
+    @SuppressLint("IntentReset")
+    private fun sendEmail(recipient: String, subject: String, message: String, file: File) {
+        val mIntent = Intent(Intent.ACTION_SEND).apply {
+            data = Uri.parse("mailto:")
+            type = "vnd.android.cursor.dir/email"
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, message)
+
+            val fileUri: Uri = FileProvider.getUriForFile(
+                requireContext(),
+                "${context?.packageName}.provider",
+                file
+            )
+            putExtra(Intent.EXTRA_STREAM, fileUri)
+        }
+
+        try {
+            startActivity(Intent.createChooser(mIntent, "Choisissez une application de messagerie"))
+        } catch (e: Exception) {
+            Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
         }
     }
 }
